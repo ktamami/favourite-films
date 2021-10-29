fe1d35823e19784510c8e1189a68dadc558b92cc
ktamami <0ge2rb5fuDtrdpw8G%g%python>
2021-10-30 07:30:11 +0900

    hide button

--- 100days_of_code/Day64-Top10films/main.py
+++ 100days_of_code/Day64-Top10films/main.py
@@ -5,12 +5,10 @@ from sqlalchemy import desc
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import URL, NumberRange
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top10films.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
