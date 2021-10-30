76532317b24221ae32c208cbee3929283e324456
ktamami <0ge2rb5fuDtrdpw8G%g%python>
2021-10-30 14:02:05 +0900

    procfile

--- 100days_of_code/Day64-Top10films/main.py
+++ 100days_of_code/Day64-Top10films/main.py
@@ -11,7 +11,7 @@ import os
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top10films.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///top10films.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 Bootstrap(app)
