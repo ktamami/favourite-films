b4afa2d43b0dc38c9c7ee6507eec9293a080bd79
ktamami <0ge2rb5fuDtrdpw8G%g%python>
2021-10-30 12:49:54 +0900

    procfile

--- 100days_of_code/Day64-Top10films/main.py
+++ 100days_of_code/Day64-Top10films/main.py
@@ -7,10 +7,11 @@ from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import URL, NumberRange
 from dotenv import load_dotenv
 from waitress import serve
+import os
 
 load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "himitsudeeeeeeeeeeeeeeeeeesu"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top10films.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
