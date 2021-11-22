e8bf2d4b1a41c08f80567611e5cd35ea5ff91360
Tamami <85936708+ktamami@users.noreply.github.com>
2021-11-22 15:33:45 +0900

    Add files via upload

--- 100days_of_code/Day64-Top10films/main.py
+++ 100days_of_code/Day64-Top10films/main.py
@@ -145,4 +145,4 @@ def logout():
     return redirect(url_for("home", admin=False))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
